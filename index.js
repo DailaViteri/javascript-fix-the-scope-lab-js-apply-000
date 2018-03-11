a8f3a8d322630678d869ed6d31fc8c3b8ad478ca
Daila Viteri <dailaviteri@gmail.com>
2018-03-11 07:06:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ function yourAnimal(dog) {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return function ()
-  return "cat"
+  return  ('cat')
 }
 
 function add2(n) {
